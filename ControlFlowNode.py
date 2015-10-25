2f9d570b286062bf8327e6f5c7103f7a1b3300d3
Dillon Huff <dillonhuff@gmail.com>
2015-10-25 02:33:47 -0700

    Deleted commented out code in ControlFlowNode

--- ControlFlowNode.py
+++ ControlFlowNode.py
@@ -8,34 +8,8 @@ class ControlFlowNode():
         self.children = set()
         self.varValues = {}
 
-    # def setNone(self, val):
-    #     self.varValues[val] = set()
-    #     self.varValues[val].add('none')
-
-    # def setNotNone(self, val):
-    #     self.varValues[val] = set()
-    #     self.varValues[val].add('not-none')
-
-    # def maybeNone(self, var):
-    #     if var in self.varValues:
-    #         self.varValues[var].add('maybe-none')
-    #     else:
-    #         self.varValues[var] = set()
-    #         self.varValues[var].add('maybe-none')
-
-    # def shouldntBeNone(self, var):
-    #     if var in self.varValues:
-    #         self.varValues[var].add('shouldnt-be-none')
-    #     else:
-    #         self.varValues[var] = set()
-    #         self.varValues[var].add('shouldnt-be-none')
-
     def newChild(self):
         newChild = ControlFlowNode(self.fileName, self.funcName)
-        # for var in self.varValues:
-        #     newChild.varValues[var] = set()
-        #     for val in self.varValues[var]:
-        #         newChild.varValues[var].add(val)
         self.children.add(newChild)
         return newChild
 
